48cea6a49ffd6c1c2485f5f45ad5f27d39baa56b
pragmaticpat <sully317@gmail.com>
2022-01-31 15:05:12 -0500

    chore: pushing code from last year

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const createCollage = require("nf-photo-collage");
+// const createCollage = require("nf-photo-collage");
 require("dotenv").config();
 const fs = require("fs");
 
@@ -34,7 +34,7 @@ console.log(process.env.GITHUB_TOKEN);
     // get contributor list from PR's
     let contributors = Array.from(nodes)
       .filter((pr) => !exclusions.includes(pr.author.login))
-      .filter((pr) => pr.mergedAt.indexOf("2020-", 0) >= 0)
+      .filter((pr) => pr.mergedAt.indexOf("2021-", 0) >= 0)
       .map((pr) => {
         return {
           author: pr.author.login,
@@ -72,7 +72,7 @@ console.log(process.env.GITHUB_TOKEN);
       contributors.push(
         ...Array.from(nodes)
           .filter((pr) => pr.author && !exclusions.includes(pr.author.login))
-          .filter((pr) => pr.mergedAt.indexOf("2020-", 0) >= 0)
+          .filter((pr) => pr.mergedAt.indexOf("2021-", 0) >= 0)
           .map((pr) => {
             return {
               author: pr.author.login,
@@ -97,15 +97,15 @@ console.log(process.env.GITHUB_TOKEN);
     console.info(
       `🎉 There were a total of ${contributors.length} PR's merged from ${
         Object.keys(uniqueAuthorsWithImages).length
-      } open source contributors in 2020 for ${process.env.GITHUB_REPO} ${
+      } open source contributors in 2021 for ${process.env.GITHUB_REPO} ${
         process.env.GITHUB_OWNER
       }`
     );
 
     // get avatars
-    const uniqueAvatars = new Set(contributors.map((c) => c.avatarUrl));
-    await saveAvatars(Array.from(uniqueAvatars));
-    await createCollageFrom("./img/", "collage.png");
+    // const uniqueAvatars = new Set(contributors.map((c) => c.avatarUrl));
+    // await saveAvatars(Array.from(uniqueAvatars));
+    // await createCollageFrom("./img/", "collage.png");
 
     const contributorsFilePath = `./contributors-${process.env.GITHUB_OWNER}-${
       process.env.GITHUB_REPO
@@ -131,59 +131,59 @@ console.log(process.env.GITHUB_TOKEN);
   }
 })();
 
-const createCollageFrom = async function (
-  imageFolderPath,
-  destinationCollageFileName
-) {
-  console.log("creating collage...");
-  await fs.promises
-    .readdir(imageFolderPath)
-    .then((files) => {
-      const images = [];
-      files.forEach((file) => {
-        const imagePath = `${imageFolderPath}/${file}`;
-        images.push(imagePath);
-      });
-      return images;
-    })
-    .then((images) => {
-      const dimensions = Math.sqrt(images.length);
-      const imagesWide = Math.round(dimensions * 1.14);
-      const imagesHigh = Math.round(dimensions * 0.87);
-      const options = {
-        sources: images,
-        width: 43, //imagesWide, //widescreen 16/25
-        height: 24, //imagesHigh, //widescreen 9/25
-        imageWidth: 50,
-        imageHeight: 50,
-      };
-      createCollage(options).then((canvas) => {
-        console.log(
-          `creating a collage that is ${imagesHigh} images high by ${imagesWide} images across`
-        );
+// const createCollageFrom = async function (
+//   imageFolderPath,
+//   destinationCollageFileName
+// ) {
+//   console.log("creating collage...");
+//   await fs.promises
+//     .readdir(imageFolderPath)
+//     .then((files) => {
+//       const images = [];
+//       files.forEach((file) => {
+//         const imagePath = `${imageFolderPath}/${file}`;
+//         images.push(imagePath);
+//       });
+//       return images;
+//     })
+//     .then((images) => {
+//       const dimensions = Math.sqrt(images.length);
+//       const imagesWide = Math.round(dimensions * 1.14);
+//       const imagesHigh = Math.round(dimensions * 0.87);
+//       const options = {
+//         sources: images,
+//         width: 43, //imagesWide, //widescreen 16/25
+//         height: 24, //imagesHigh, //widescreen 9/25
+//         imageWidth: 50,
+//         imageHeight: 50,
+//       };
+//       // createCollage(options).then((canvas) => {
+//       //   console.log(
+//       //     `creating a collage that is ${imagesHigh} images high by ${imagesWide} images across`
+//       //   );
 
-        const src = canvas.jpegStream();
-        const dest = fs.createWriteStream(destinationCollageFileName);
-        src.pipe(dest);
-      });
-    })
-    .catch((err) => console.log(`💩 uhhh... ${err}`));
-};
+//       //   const src = canvas.jpegStream();
+//       //   const dest = fs.createWriteStream(destinationCollageFileName);
+//       //   src.pipe(dest);
+//       // });
+//     })
+//     .catch((err) => console.log(`💩 uhhh... ${err}`));
+// };
 
-const saveAvatars = async function (listOfAvatarUrls) {
-  console.log(`saving ${listOfAvatarUrls.length} avatars`);
-  const progressAvatars = new cliProgress.SingleBar(
-    {},
-    cliProgress.Presets.shades_classic
-  );
+// // const saveAvatars = async function (listOfAvatarUrls) {
+// //   console.log(`saving ${listOfAvatarUrls.length} avatars`);
+// //   const progressAvatars = new cliProgress.SingleBar(
+// //     {},
+// //     cliProgress.Presets.shades_classic
+// //   );
 
-  progressAvatars.start(listOfAvatarUrls.length, 0);
-  for (let index = 0; index < listOfAvatarUrls.length; index++) {
-    const avatar = listOfAvatarUrls[index];
-    const buffer = await getAvatar(avatar);
-    fs.writeFile(`./img/${Date.now()}.png`, buffer, () =>
-      progressAvatars.increment()
-    );
-  }
-  progressAvatars.stop();
-};
+// //   progressAvatars.start(listOfAvatarUrls.length, 0);
+// //   for (let index = 0; index < listOfAvatarUrls.length; index++) {
+// //     const avatar = listOfAvatarUrls[index];
+// //     const buffer = await getAvatar(avatar);
+// //     fs.writeFile(`./img/${Date.now()}.png`, buffer, () =>
+// //       progressAvatars.increment()
+// //     );
+// //   }
+// //   progressAvatars.stop();
+// };
